652e4d68f35918fdb4bc90c602e699dba989ac91
volkovskiyda <volkovskiyda@gmail.com>
2024-02-07 13:22:31 +0200

    fix ydl option extract_flat

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ ydl_opts = {
     'format': 'mp4',
     'outtmpl': 'videos/%(upload_date)s - %(title).40s [%(id).20s].%(ext)s',
     'noplaylist': True,
-    'extract_flat': True,
+    'extract_flat': 'discard_in_playlist',
     'playlist_items': '1-20',
     'match_filter': is_not_live,
     'quiet': True,
