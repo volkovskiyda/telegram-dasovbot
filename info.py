0485f7b9c9909b59c60675f775b8dd8d376b10eb
volkovskiyda <volkovskiyda@gmail.com>
2024-08-27 12:23:23 +0300

    info: add additional fields

--- info.py
+++ info.py
@@ -12,10 +12,12 @@ def json_dumps(info):
 
 def video(info):
     title = info['title']
+    description = info['description']
     url = info.get('webpage_url') or info['url']
     duration = int(info.get('duration') or 0)
-    download = info.get('url')
     format = info.get('format')
+    filesize = sizeof_fmt(int(info.get('filesize') or 0))
+    download = info.get('url')
     thumbnail = info.get('thumbnail')
     upload_date = info.get('upload_date')
     channel_url = info['channel_url']
@@ -24,12 +26,15 @@ def video(info):
     live_status = info['live_status']
     is_live = info.get('is_live')
     was_live = info.get('was_live')
+    entries = info.get('entries')
     video = {
         'title': title,
+        'description': description[:50],
         'url': url,
         'duration': duration,
         'download': download,
         'format': format,
+        'filesize': filesize,
         'thumbnail': thumbnail,
         'upload_date': upload_date,
         'channel_url': channel_url,
@@ -38,6 +43,7 @@ def video(info):
         'live_status': live_status,
         'is_live': is_live,
         'was_live': was_live,
+        'entries': entries,
     }
     return video
 
@@ -55,6 +61,13 @@ async def info(query: str, download: bool) -> None:
     
     json_dumps(output)
 
+def sizeof_fmt(num, suffix="B"):
+    if (num == 0.0): return "N/A"
+    for unit in ("", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"):
+        if abs(num) < 1024.0:
+            return f"{num:3.1f}{unit}{suffix}"
+        num /= 1024.0
+    return f"{num:.1f}Yi{suffix}"
 
 def main() -> None:
     parser = argparse.ArgumentParser()
