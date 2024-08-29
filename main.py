d11882fe7945125c4f3d27f28b15cc3f699171cc
volkovskiyda <volkovskiyda@gmail.com>
2024-08-29 11:07:01 +0300

    filter_entries: live_status

--- main.py
+++ main.py
@@ -68,10 +68,16 @@ def remove_command_prefix(command: str) -> str:
 
 def process_entries(entries: list) -> list:
     nested_entries = entries[0].get('entries')
-    return nested_entries if nested_entries else filter_duration(entries)
+    return nested_entries if nested_entries else filter_entries(entries)
 
-def filter_duration(entries: list) -> list:
-    return list(filter(lambda entry: entry.get('duration'), entries))
+def filter_entries(entries: list) -> list:
+    return list(filter(lambda entry: entry.get('duration') and entry.get('live_status') is None, entries))
+
+# def filter_duration(entries: list) -> filter:
+#     return filter(lambda entry: entry.get('duration'), entries)
+
+# def filter_status(entries: list) -> filter:
+#     return filter(lambda entry: entry.get('live_status') is None, entries)
 
 def extract_user(user: User) -> str:
     return f"{now()} {user.username} ({user.id})"
@@ -177,7 +183,7 @@ def populate_playlist(channel: str, chat_ids: list):
     if not entries:
         print(f"{now()} # populate_playlist no entries: {channel}")
         return
-    for entry in filter_duration(entries)[:5]: populate_video(entry, chat_ids)
+    for entry in filter_entries(entries)[:5]: populate_video(entry, chat_ids)
 
 def populate_video(entry: dict, chat_ids: list):
     query = extract_url(entry)
