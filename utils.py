ec1414d56d68d5311cc0793fcdd83a9ccbd9c921
volkovskiyda <volkovskiyda@gmail.com>
2024-09-04 11:55:03 +0300

    fix format_sort

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ def match_filter(info, *, incomplete):
     if info.get('is_live'): return f"{now()} # ignore video {info.get('url')}"
 
 ydl_opts = {
-    'format_sort': 'res:720,vcodec,ext',
+    'format_sort': 'res:720',
     'outtmpl': 'videos/%(upload_date)s - %(title).40s [%(id).20s].%(ext)s',
     'noplaylist': True,
     'extract_flat': 'in_playlist',
