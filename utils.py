7e2089bcf5ffce5654c845aa95d02f2ff08ac949
volkovskiyda <volkovskiyda@gmail.com>
2024-12-27 14:19:00 +0200

    ydl_opts postprocessors: add chapters and metadata

--- utils.py
+++ utils.py
@@ -18,6 +18,7 @@ ydl_opts = {
     'match_filter': match_filter,
     'no_warnings': True,
     'quiet': True,
+    'postprocessors': [{'key': 'FFmpegMetadata'}],
 }
 
 def extract_url(info: dict) -> str:
